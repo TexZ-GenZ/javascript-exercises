92279b462e38d28ae6aead429647428f8608ebfb
TexZ-GenZ <priyanshusamal21@gmail.com>
2024-06-08 18:02:02 +0530

    Completed excercise 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let tempInCelsius = (tempInFahrenheit - 32)*(5/9);
+
+  if (Math.floor(tempInCelsius) === tempInCelsius) {
+    return tempInCelsius;
+  }
+  return Number(tempInCelsius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let tempInFahrenheit = (9/5)*(tempInCelsius) + 32;
+
+  if (Math.floor(tempInFahrenheit) === tempInFahrenheit) {
+    return tempInFahrenheit;
+  }
+  return Number(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
